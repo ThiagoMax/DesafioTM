b9471f3d3000ce582c443db4fad1be1248184894
ThiagoMax <thiagomaximiliano21@gmail.com>
2022-10-06 14:11:12 -0300

    Criação das entidades e sub-entidades

---
+++ DesafioTM/Model/Event.cs
@@ -0,0 +1,18 @@
+﻿using DesafioTM.Model.Base;
+using DesafioTM.Model.Perfil;
+using System;
+
+namespace DesafioTM.Model
+{
+    public class Event : BaseEntity
+    {
+        public string Name { get; set; }
+        public string Location { get; set; }
+        public decimal Price { get; set; }
+        public string Description { get; set; }
+        public DateTime Date { get; set; }
+        public int TicketsQt { get; set; }
+        public string Type { get; set; }
+        public Organizer OrganizerName { get; set; }
+    }
+}
