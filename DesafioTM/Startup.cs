dae207a95b5227e07fa45fd4b3f5f12010ef8885
ThiagoMax <thiagomaximiliano21@gmail.com>
2022-10-06 18:53:24 -0300

    Criado a conexão com banco de dados

--- DesafioTM/Startup.cs
+++ DesafioTM/Startup.cs
@@ -1,7 +1,9 @@
+using DesafioTM.Model.Context;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -27,6 +29,9 @@ namespace DesafioTM
         {
 
             services.AddControllers();
+
+            var connection = Configuration["MySQLConnection:MySQLConnectionString"];
+            services.AddDbContext<MySQLContext>(options => options.UseMySql(connection, ServerVersion.AutoDetect(connection)));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
